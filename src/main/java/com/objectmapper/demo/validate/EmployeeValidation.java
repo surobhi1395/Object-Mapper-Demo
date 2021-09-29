7b25a33c55436acdf8de9848672ed2ed84b2894c
Surobhi <surobhi1395@gmail.com>
2021-09-29 20:53:06 +0530

    deleted unnecessary class

--- src/main/java/com/objectmapper/demo/validate/EmployeeValidation.java
+++ src/main/java/com/objectmapper/demo/validate/EmployeeValidation.java
@@ -1,7 +1,6 @@
 package com.objectmapper.demo.validate;
 
 import com.objectmapper.demo.constant.EmployeeConstant;
-import com.objectmapper.demo.exception.BadRequestException;
 import com.objectmapper.demo.exception.EmployeeNotFoundException;
 import com.objectmapper.demo.model.EmployeeData;
 import org.springframework.stereotype.Component;
