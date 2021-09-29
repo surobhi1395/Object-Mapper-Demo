5ec1a1539e215d8c54a2df7f535eb09ec0f1291e
Surobhi <surobhi1395@gmail.com>
2021-09-29 18:58:43 +0530

    changes made in controller

--- src/main/java/com/objectmapper/demo/controller/EmployeeController.java
+++ src/main/java/com/objectmapper/demo/controller/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
         return employeeService.getAllEmployee();
     }
 
-    @PostMapping("/createEmployee")
+    @PostMapping("/add")
     public EmployeeData addEmployee(@RequestBody EmployeeData employeeData){
         return employeeService.addEmployee(employeeData);
     }
