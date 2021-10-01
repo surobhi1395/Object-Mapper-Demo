fd08e387c4b7dbe13390502726422208a1eb4a08
Surobhi <surobhi1395@gmail.com>
2021-10-01 10:00:05 +0530

    validation changes

--- src/main/java/com/objectmapper/demo/validate/SalaryValidation.java
+++ src/main/java/com/objectmapper/demo/validate/SalaryValidation.java
@@ -6,24 +6,29 @@ import org.springframework.stereotype.Component;
 @Component
 public class SalaryValidation {
 
-    public void checkSalIncrement(EmployeeData employeeData){
+    public EmployeeData checkSalIncrement(EmployeeData employeeData){
         int increment = 0;
-        if(employeeData.getEmployeeSalary()>300000){
+        if(employeeData.getEmployeeSalary()>300000
+            && employeeData.getEmployeeSalary()<500000){
             increment = (int) (employeeData.getEmployeeSalary() +
                                 (employeeData.getEmployeeSalary() * 0.5));
             employeeData.setEmployeeSalary(increment);
+            return employeeData;
         }
-        if(employeeData.getEmployeeSalary()>200000){
+        if(employeeData.getEmployeeSalary()>200000
+            && employeeData.getEmployeeSalary()<300000){
             increment = (int) (employeeData.getEmployeeSalary() +
                     (employeeData.getEmployeeSalary()*0.2));
             employeeData.setEmployeeSalary(increment);
+            return employeeData;
         }
         if(employeeData.getEmployeeSalary()>500000){
             increment = (int) (employeeData.getEmployeeSalary() +
                     (employeeData.getEmployeeSalary()*0.02));
             employeeData.setEmployeeSalary(increment);
+            return employeeData;
         }
-
+      return employeeData;
     }
 
 }
